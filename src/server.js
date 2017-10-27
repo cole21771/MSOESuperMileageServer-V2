55b55c2af020dd3596880219d82c0858dcdd1869
Cole Abbeduto <abbedutocg@msoe.edu>
2017-10-27 13:35:54 -0500

    Moved server.js into src since that made sense

--- src/server.js
+++ src/server.js
@@ -11,11 +11,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('../dist'));
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile('../dist/index.html');
 });
 
 let port = 3000;
