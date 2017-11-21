2204d41e19c07dde62beb8c0904b038d08453d57
Cole Abbeduto <abbedutocg@msoe.edu>
2017-11-21 14:46:14 -0600

    Fixed lots of problems

--- src/server.js
+++ src/server.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const app = express();
 app.use(require('compression')());
+
 const server = require('http').Server(app);
+const io = require('socket.io').listen(server);
+const path = require('path');
 
 // Parsers for POST data
 const bodyParser = require('body-parser');
@@ -10,25 +13,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Point static path to dist
-app.use(express.static('../dist'));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile('../dist/index.html');
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
 let port = 3000;
 server.listen(port, () => console.log(`Listening on port ` + port));
 
 /********************Socket.IO********************************************************/
-let io = require('socket.io').listen(server);
-
 io.sockets.on('connection', (socket) => {
   socket.on('newData', () => {
     //Do something when receiving new data
   });
 
-  socket.on('OpenAllSidenavs', () => {
-    io.emit('OpenSidenav');
-  });
+
 });
