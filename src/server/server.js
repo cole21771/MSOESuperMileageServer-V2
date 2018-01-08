b16c6a334c40fb85b5ee387a17ade2d623cc6f02
Cole Abbeduto <abbedutocg@msoe.edu>
2018-01-08 13:20:55 -0600

    Reverted to http?

--- src/server/server.js
+++ src/server/server.js
@@ -16,34 +16,17 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Point static path to dist
 app.use(express.static(path.join(__dirname, '../../dist')));
 
-app.use((req, res, next) => {
-  if(req.secure) {
-    next();
-  } else {
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
-
 // Catch all other routes and return the index file
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
 });
 
-const options = {
-  /*key: fs.readFileSync(path.join(__dirname, 'certificates/server.key')),
-  cert: fs.readFileSync(path.join(__dirname, 'certificates/server.crt')),*/
-  spdy: {
-    plain: true,
-    ssl: false
-  }
-};
-
-const http2 = require('spdy').createServer(options, app);
-const io = require('socket.io').listen(http2);
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 require('./socket-io.events')(io, fs, logger);
 
 const port = 3000;
-http2.listen(port, (err) => {
+server.listen(port, (err) => {
   if (err)
     throw err;
   console.log(`Listening on port ` + port)
