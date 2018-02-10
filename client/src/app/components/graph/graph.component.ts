2b7204232ef831e8d571d0bcc68167759df040fe
Cole Abbeduto <abbedutocg@msoe.edu>
2018-02-10 11:04:24 -0600

    Fixed a problem with production not running due to a command syntax error.

--- client/src/app/components/graph/graph.component.ts
+++ client/src/app/components/graph/graph.component.ts
@@ -16,18 +16,18 @@ export class GraphComponent implements OnInit {
   }
 
   ngOnInit() {
-      if (this.graphInfo.isValid) {
-        this.dataService.dataNotifier.subscribe(() => {
-          const x = this.dataService.getLatestData(this.graphInfo.xLabel);
-          const y = this.dataService.getLatestData(this.graphInfo.yLabel);
-          if (isNaN(x) || isNaN(y)) {
-            throw new Error(`${this.graphInfo.title} encountered a problem with getting data from DataService`);
-          } else {
-            this.graphInfo.addData(x, y);
-          }
-        });
-      } else {
-        throw new Error('Graph Information is not valid!');
-      }
+    if (this.graphInfo.isValid) {
+      this.dataService.dataNotifier.subscribe(() => {
+        const x = this.dataService.getLatestData(this.graphInfo.xLabel);
+        const y = this.dataService.getLatestData(this.graphInfo.yLabel);
+        if (isNaN(x) || isNaN(y)) {
+          throw new Error(`${this.graphInfo.title} encountered a problem with getting data from DataService`);
+        } else {
+          this.graphInfo.addData(x, y);
+        }
+      });
+    } else {
+      throw new Error('Graph Information is not valid!');
+    }
   }
 }
