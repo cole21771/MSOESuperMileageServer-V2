249e47e6667cae6b4ffda95baf9832ba4085c763
Cole Abbeduto <abbedutocg@msoe.edu>
2018-04-16 21:47:13 -0500

    Add basic polyline to map

--- client/src/app/components/tiles/map/map.component.ts
+++ client/src/app/components/tiles/map/map.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {MapProperties} from '../../../models/interfaces/config/MapProperties';
 import {DataService} from '../../../services/data/data.service';
+import {LocationInfo} from '../../../models/interfaces/LocationInfo';
 
 @Component({
   selector: 'app-map',
@@ -9,14 +10,27 @@ import {DataService} from '../../../services/data/data.service';
 })
 export class MapComponent implements OnInit {
   @Input() mapProperties: MapProperties;
+  public locations: LocationInfo[];
 
   constructor(private dataService: DataService) {
+    this.locations = [];
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.dataService.locationNotifier.subscribe((locationInfo) => {
 
     });
+
+    this.locations.push({lat: this.mapProperties.lat, long: this.mapProperties.long, speed: 10});
+    for (let i = 1; i < 200; i++) {
+      const lastLoc = this.locations[i - 1];
+      this.locations.push({
+        lat: lastLoc.lat + (Math.random() - .5) / 1000,
+        long: lastLoc.long + (Math.random() - .5) / 1000,
+        speed: 10
+      });
+      await new Promise(resolve => setTimeout(resolve, 500));
+    }
   }
 
 }
